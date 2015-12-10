cb5638d0a78075a10bb479aa1723a3a359639b61
Tom Wicks <tom@miln.co>
2015-12-10 19:20:03 +0000

    Home Page Start

--- includes/global/footer.php
+++ includes/global/footer.php
@@ -1,3 +1,16 @@
+<script type="text/javascript" src="js/plugins/swiper.js"></script>
+
+<script>
+    var swiper = new Swiper('.swiper-container', {
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 'auto',
+    	centeredSlides: true,
+		spaceBetween: 0
+    });
+</script>
+
 <script type="text/javascript" src="js/lib/jquery.js"></script>
 <script type="text/javascript" src="js/global.js"></script>
 
--- index.php
+++ index.php
@@ -1,16 +1,237 @@
 <?php include("includes/global/header.php"); ?>
 
 
+	<header>
+		
+		<div class="grid-container">
 
-	<div class="grid-container">
+			<div class="row">
 
-		<header>
+				<div class="col-4">
 
+					<a href="/" title="Return to the homepage" class="header-logo">
+					  <!-- Need correct Logo <img src="img/purpledog_logo.svg" alt="Purple Dog Logo" /> -->
+					</a>
+
+				</div>
+
+				<div class="col-right col-6">
+
+					<ul class="shop-nav">
+
+						<li><a href="">First Aid Calculator</a></li>
+						<li><a href="">Login</a></li>
+						<li><a href="">My Account</a></li>
+
+					</ul>
+
+					<ul class="cta-nav">
+						<li><h5>01484 546780</h5></li>
+						<li><a class="link-btn-2">Book Online</a></li>
+					</ul>
+
+				</div>
+
+			</div>
+
+		</div>
+
+		<nav>
+
+			<div class="grid-container">
+
+					<ul>
+						<li class="active"><a>Home</a></li>
+						<li><a class="dropdown">About</a></li>
+						<li><a>News</a></li>
+						<li><a class="dropdown">Courses</a></li>
+						<li><a>Contact</a></li>
+					</ul>
+
+					<ul class="nav-right">
+						<li><a class="basket">Basket</a></li>
+						<li><a class="facebook"></a></li>
+						<li><a class="twitter"></a></li>
+					</ul>
+			</div>
+
+		</nav>
+
+	</header>
+
+	<section class="home-splash">
+
+		<div class="grid-container">
+
+			<div class="row">
+
+				<div class="col-7">
+
+					<div class="pd-speech">
+
+						<h2 class="underline">The training from Purple Dog helped me save a 18 month year old girls life.</h2>
+
+					</div>
+
+				</div>
+
+				<div class="col-5 col-right">
+
+					<img src="img/banner_mother.png" alt="Mother and Daughter" />
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</section>
+
+	<section class="home-description">
+
+		<div class="grid-container">
+
+			<div class="row">
+
+				<div class="col-10">
+
+					<h4 class="underline">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </h4>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</section>
+
+	<section class="home-courses">
+
+		<div class="grid-container">
+
+			<div class="row">
+
+				<h3>Courses</h3>
+
+			</div>
+
+			<div class="row">
+
+				<div class="course-boxes">
+
+					<a class="link-course-box">
+
+						<img src="" alt=""/>
+
+						<div class="title">
+
+							<h5>First Aid</h5>
+
+						</div>
+
+					</a>
+
+					<a class="link-course-box">
+
+						<img src="" alt=""/>
+
+						<div class="title">
+
+							<h5>First Aid</h5>
+
+						</div>
+
+					</a>
+
+					<a class="link-course-box">
+
+						<img src="" alt=""/>
+
+						<div class="title">
+
+							<h5>First Aid</h5>
+
+						</div>
+
+					</a>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</section>
+
+	<section class="home-calculator">
+
+		<div class="grid-container">
+
+			<div class="row">
+
+				<h3>First Aid Calculator</h3>
+
+			</div>
+
+			<div class="row">
+
+				<div class="calculator-splash">
+
+					<div class="col-5">
+
+						<h3>Find out how many First Aiders you need with our First Aid calculator</h3>
+
+						<a class="link-btn-3">Find Out How</a>
+					
+					</div>
+
+					<div class="col-7">
+
+						<img src="img/calculator.png" alt=""/>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</section>
+
+	<section class="home-testimonials">
+
+		<div class="grid-container">
+
+			<div class="row">
+
+				<h3>Testimonials</h3>
+
+			</div>
+
+			<div class="swiper-container">
+
+	        <div class="swiper-wrapper">
+	            
+	            <div class="swiper-slide">
+
+	            	<h4 class="underline">The tutor was engaging, funny, intelligent and everything was interesting. I wasn’t bored even for a second!</h4>
+
+	            </div>
+
+	        </div>
+
+	        <!-- Add Arrows -->
+	        <div class="swiper-button-next"></div>
+	        <div class="swiper-button-prev"></div>
+	    </div>
 			
 
-		</header>
+		</div>
 
-	</div>
+	</section>
+
+	
 
 
 
